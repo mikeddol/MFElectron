21af14d8b27a32f28d3ff9224d58d5756cd1b038
akagetsu <mike@akagetsu.com>
2017-03-01 21:55:53 +0200

    Add in some basic csv parsing and info retrieval

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 //jshint esversion:6
-const api = require('./api/api');
-const cod = "";
+const fs = require('fs'),
+  csv = require('fast-csv');
 
-api.getInfo(cod)
-  .then(function (res) {
-    console.log(res);
-  }).catch(function (err) {
-    console.error(err);
+const api = require('./api/api');
+let codes = [];
+let stream = fs.createReadStream("clienti.csv");
+
+let csvStream = csv
+  .parse()
+  .on("data", function (data) {
+    codes.push(data.pop());
+  }).on("end", function () {
+    codes.shift();
+    getLocations(codes);
   });
+stream.pipe(csvStream);
+
+
+
+const getLocations = function (codes) {
+  const locations = codes.map(function (cod) {
+    return api.getInfo(cod)
+      .then(function (res) {
+        return res;
+      }).catch(function (err) {
+        console.error(err);
+      });
+  });
+
+  Promise.all(locations).then(function (values) {
+    console.log(values);
+  });
+};
