e77c67f73a49d952e3126344a366d008698c66ce
akagetsu <mike@akagetsu.com>
2017-03-05 11:38:13 +0200

    Set up method to set inputpath beforehand

--- app/app.js
+++ app/app.js
@@ -5,28 +5,44 @@ const app = (function () {
     path = require('path'),
     api = require('./api/api');
 
-  const run = function (inputPath) {
-    let codes = [];
-    const startPath = path.resolve(inputPath);
+  let inputPath = '';
 
-    const parsedPath = path.parse(inputPath);
-    const endPath = path.resolve(parsedPath.dir, parsedPath.name + '_filled' + parsedPath.ext);
+  const setInputPath = function (userInputPath) {
+    return new Promise(function (resolve, reject) {
+      if (!userInputPath && userInputPath.length === 0) {
+        reject("Invalid input path!");
+      }
+      inputPath = userInputPath;
+      resolve();
+    });
+  };
 
-    csv.fromPath(startPath)
-      .on("data", function (data) {
-        codes.push(data.pop());
-      }).on("end", function () {
-        codes.shift();
+  const run = function () {
+    return new Promise(function (resolve, reject) {
+      let codes = [];
+      const startPath = path.resolve(inputPath);
 
-        getData(codes)
-          .then(function (fullData) {
-            return setData(fullData, endPath);
-          })
-          .then(function () {
-            console.log("done!");
-          });
+      const parsedPath = path.parse(inputPath);
+      const endPath = path.resolve(parsedPath.dir, parsedPath.name + '_filled' + parsedPath.ext);
 
-      });
+      csv.fromPath(startPath)
+        .on("data", function (data) {
+          codes.push(data.pop());
+        }).on("end", function () {
+          codes.shift();
+
+          getData(codes)
+            .then(function (fullData) {
+              return setData(fullData, endPath);
+            })
+            .then(function (endPath) {
+              resolve(endPath);
+            })
+            .catch(function (err) {
+              reject(err);
+            });
+        });
+    });
   };
 
   const getData = function (codes) {
@@ -52,13 +68,14 @@ const app = (function () {
       csv.writeToPath(endPath, fullData, {
         headers: true
       }).on('finish', function () {
-        resolve();
+        resolve(endPath);
       });
     });
   };
 
   return {
-    run: run
+    run: run,
+    setInputPath: setInputPath
   };
 })();
 
